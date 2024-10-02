8a00af82e242b0d8bdd01ae16c7429793c8d090a
Kato <phantompiratefrizy@gmail.com>
2024-10-02 11:15:51 +0800

    Minor changes on blog page

--- app/Blogs/Blog7/page.tsx
+++ app/Blogs/Blog7/page.tsx
@@ -29,15 +29,14 @@ const blogPosts = [
 const Page = () => {
   return (
     <div className="mt-4 mx-auto max-w-4xl font-roboto">
-      <p className="text-[#FEA13E] text-lg font-bold mb-4">
-        Young Engineers Mongolia
-      </p>
-      <h1 className="text-[#585858] text-3xl font-semibold mb-4">
-        This is Phantom, a free, fully responsive site template designed by HTML5 UP.
-      </h1>
-      <p className="text-sm text-gray-500 mb-8">
-        Etiam quis viverra lorem, in semper lorem. Sed nisl arcu euismod sit amet nisi euismod sed cursus arcu elementum ipsum arcu vivamus quis venenatis orci lorem ipsum et magna feugiat veroeros aliquam. Lorem ipsum dolor sit amet nullam dolore.
-      </p>
+      <div className="flex flex-col justify-center text-center">
+        <h1 className="text-[#585858] text-3xl font-semibold mb-4">
+          This is Phantom, a free, fully responsive site template designed by HTML5 UP.
+        </h1>
+        <p className="text-sm text-gray-500 mb-8">
+          Etiam quis viverra lorem, in semper lorem. Sed nisl arcu euismod sit amet nisi euismod sed cursus arcu elementum ipsum arcu vivamus quis venenatis orci lorem ipsum et magna feugiat veroeros aliquam. Lorem ipsum dolor sit amet nullam dolore.
+        </p>
+      </div>
       <Card className="p-6 bg-gray-200">
         <div className="">
           {blogPosts.map((post, index) => (
